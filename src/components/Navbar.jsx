e5703cb9415062dfd50f050b2048fb948fd1dbd8
aadil124 <ansariaadil124@gmail.com>
2023-07-18 14:55:37 +0530

    nav bar aaded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+   <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">My Movies</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">About</a>
+        </li>
+      </ul>
+     <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search"/>
+      </form>
+    </div>
+  </div>
+</nav>
   )
 }
 
