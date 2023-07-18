9646912d5a72d71907b45cad4b8a020154a6106c
aadil124 <ansariaadil124@gmail.com>
2023-07-19 00:35:11 +0530

    card data added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({data}) => {
+  console.log(data,"+++++++")
   return (
-    <div>MovieCard</div>
+  <div className="row row-cols-1 row-cols-md-4 g-3 mt-2">
+     { data.map((card,index)=> {
+      return <div className="col" key={index}>
+    <div className="card h-80">
+      <img src={card.posterUrl} className="card-img-top h-100" alt={card.title}/>
+      <div className="card-body text-center">
+        <h3 className="card-title">{card.title}</h3>
+        <h4 className="card-title">{card.year}</h4>
+      </div>
+    </div>
+  </div>
+      })}
+    
+   </div>
   )
 }
 
