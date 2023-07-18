c33eb7d7422e9c721ff4b58d7a083a62fc89bb19
aadil124 <ansariaadil124@gmail.com>
2023-07-19 00:35:34 +0530

    added padding to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-   <div class="container-fluid">
-    <a class="navbar-brand text-light" href="#">My Movies</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-4">
+   <div className="container-fluid">
+    <a className="navbar-brand text-light" href="#">My Movies</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-light" href="#">About</a>
+        <li className="nav-item">
+          <a className="nav-link text-light" href="#">About</a>
         </li>
       </ul>
-     <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search"/>
+     <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search..." aria-label="Search"/>
       </form>
     </div>
   </div>
