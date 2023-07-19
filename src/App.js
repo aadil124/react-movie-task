1748376a149a449ed7423ec80a2e1434ddea418e
aadil124 <it.aadil124@gmail.com>
2023-07-19 12:45:03 +0530

    filter logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import MovieCard from './components/MovieCard';
@@ -5,11 +6,22 @@ import Navbar from './components/Navbar';
 import data from "./data/data.json"
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setSearchValue(() => e.target.value)
+  }
+  // filter logic 
+  const filterCardData = () => {
+    return data.filter((item) =>
+      item.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) || searchValue === ""
+    )
+  }
   return (
     <div>
-     <Navbar/>
-     <MovieCard data={data}/>
-     <Footer/>
+      <Navbar handleChange={handleChange} />
+      <MovieCard data={data} filterCardData={filterCardData} />
+      <Footer />
     </div>
   );
 }
