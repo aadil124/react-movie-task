2851fbd96a3659c6f600c647084143d1ab2eb9fa
aadil124 <it.aadil124@gmail.com>
2023-07-19 12:52:56 +0530

    updated code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({ handleChange }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-4">
-   <div className="container-fluid">
-    <a className="navbar-brand text-light" href="#">My Movies</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarText">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-light" href="#">About</a>
-        </li>
-      </ul>
-     <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search..." aria-label="Search"/>
-      </form>
-    </div>
-  </div>
-</nav>
+      <div className="container-fluid">
+        <a className="navbar-brand text-light" href="/">My Movies</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="/">About</a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search..." aria-label="Search" onChange={handleChange} />
+          </form>
+        </div>
+      </div>
+    </nav>
   )
 }
 
