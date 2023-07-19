ef34b193f871e1eeb99af199594caf28d8c406fe
aadil124 <it.aadil124@gmail.com>
2023-07-19 12:52:29 +0530

    filter card added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const MovieCard = ({data}) => {
-  console.log(data,"+++++++")
+const MovieCard = ({ data, filterCardData }) => {
+
   return (
-  <div className="row row-cols-1 row-cols-md-4 g-3 mt-2">
-     { data.map((card,index)=> {
-      return <div className="col" key={index}>
-    <div className="card h-80">
-      <img src={card.posterUrl} className="card-img-top h-100" alt={card.title}/>
-      <div className="card-body text-center">
-        <h3 className="card-title">{card.title}</h3>
-        <h4 className="card-title">{card.year}</h4>
-      </div>
+    <div className="row row-cols-1 row-cols-md-3 g-4 mt-2">
+      {filterCardData().map((card, index) => (
+        <>
+          <div className="col" key={index}>
+            <div className="card h-80">
+              <img src={card.posterUrl} className="card-img-top h-100" alt={card.title} />
+              <div className="card-body text-center">
+                <h3 className="card-title">{card.title}</h3>
+                <h4 className="card-title">{card.year}</h4>
+              </div>
+            </div>
+          </div>
+        </>
+      )
+      )}
     </div>
-  </div>
-      })}
-    
-   </div>
   )
 }
 
-export default MovieCard
+export default MovieCard;
